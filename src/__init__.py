0d93e951c80405961ebfa6cd1b66e616536340cd
BDPP-3942 <brayanpp012@gmail.com>
2026-08-26 21:09:23 +0200

    refactor: remove package-level naming monkey patch

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-from src.file_naming import FileNameFormatter
-from src.naming_policy import resolve as _resolve_naming
-
-
-# Keep legacy callers compatible while making the new naming policy authoritative.
-FileNameFormatter.resolve_source_metadata = classmethod(
-    lambda cls, source, extract_root: _resolve_naming(source, extract_root)
-)
