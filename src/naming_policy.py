27d15ad435107d243244b432d3903a4497c9daa0
BDPP-3942 <brayanpp012@gmail.com>
2026-08-28 15:14:34 +0200

    style(naming): apply Ruff formatting to generic names

--- src/naming_policy.py
+++ src/naming_policy.py
@@ -25,8 +25,26 @@ _LESSON_NUMBER = re.compile(
     re.IGNORECASE,
 )
 _GENERIC = {
-    "mp4", "wmv", "video", "videos", "audio", "media", "file", "files", "archivo", "archivos",
-    "download", "downloads", "descarga", "descargas", "compressed", "compression", "archive", "zip", "rar", "7z",
+    "mp4",
+    "wmv",
+    "video",
+    "videos",
+    "audio",
+    "media",
+    "file",
+    "files",
+    "archivo",
+    "archivos",
+    "download",
+    "downloads",
+    "descarga",
+    "descargas",
+    "compressed",
+    "compression",
+    "archive",
+    "zip",
+    "rar",
+    "7z",
 }
 
 
